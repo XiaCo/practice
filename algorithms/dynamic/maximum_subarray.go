cebe4ff5098560323bbc4cc5dfb6c6de12d2438c
XiaCo <619944431@qq.com>
2021-06-09 15:16:36 +0800

    53. 最大子序和

---
+++ algorithms/dynamic/maximum_subarray.go
@@ -0,0 +1,22 @@
+package dynamic
+
+// https://leetcode-cn.com/problems/maximum-subarray/
+
+// f(i)表示nums[:i]的子串最大和
+// f(i) = max{f(i-1)+nums[i], nums[i]}
+
+// 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+// 输出：6
+// 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+func maxSubArray(nums []int) int {
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i]+nums[i-1] > nums[i] {
+			nums[i] += nums[i-1]
+		}
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	return max
+}
